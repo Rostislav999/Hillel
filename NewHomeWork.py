f6bb118d7d37fc88e962dbf912206a61148eeffc
Rostislav999 <85948211+Rostislav999@users.noreply.github.com>
2022-02-15 15:03:42 +0200

    Add files via upload

---
+++ NewHomeWork.py
@@ -0,0 +1,32 @@
+first = 10
+second = 30
+sum = first + second
+diff1 = first - second
+diff2 = second - first
+mult = first * second
+div1 = first / second
+div2 = second / first
+exp1 = first ** second
+exp2 = second ** first
+remofdiv1 = first % second
+remofdiv2 = second % first
+intdiv1 = first // second
+intdiv2 = second // first
+
+print(sum)
+print(diff1)
+print(diff2)
+print(mult)
+print(div1)
+print(div2)
+print(exp1)
+print(exp2)
+print(remofdiv1)
+print(remofdiv2)
+print(intdiv1)
+print(intdiv2)
+
+
+# Работаю ветеринаром, так получилось, что пришлось переехать в Одессу)
+# Давно хотел освоить этот язык программирования, очень долго тянул и вот настало время,
+# именно менять полностью профессию пока нет в планах, но как бы все может быть
